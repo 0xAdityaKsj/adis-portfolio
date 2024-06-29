cabb8ffaa312725ca57b46b7b9570a18659fc92a
Sai Jyothir Aditya Kantipudi <106384725+0xAdityaKsj@users.noreply.github.com>
2024-06-29 11:23:24 +0530

    moonlight

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,9 +4,9 @@ function About() {
     return (
         <Box className="about" position="absolute" top="340" left="500" zIndex="10" transform="translate(-50%, -50%)" color="#FFFFFF" css={{ animation: 'fadeIn 2s ease-in-out forwards' }}
 
-            fontStyle=" sans-serif">
+            fontStyle=" sans-serif" width="80%" height="80%">
             <VStack spacing={3} align="left">
-                <Text as="h1">About Adi K</Text>
+                <Text as="h2" fontSize="lg" fontWeight="bold">About Adi K</Text>
                 <Text>
                     I appreciate beautiful designs, be it a webpage, architecture, or spacecraft..
                     I like anything that is beautiful, and I believe we should build beautiful things.
@@ -14,14 +14,17 @@ function About() {
                     I would like to learn more about AI systems, robotics, and low-level engineering.
                 </Text>
 
-                <Text as="h1" size="lg">Now</Text>
+
+                <Text as="h2" fontSize="lg" fontWeight="bold">Now</Text>
 
                 <Text>learning bout more web stuff, design ⚜️ and electronics</Text>
                 <Text>doing -
                     <Link href="https://missing.csail.mit.edu/" isExternal> <span>./missing_sem</span></Link></Text>
                 <Text>Learning Japanese | Hiragana</Text>
 
-                <Text as="h1" size="lg">Contact</Text>
+                <iframe style={{ borderRadius: '12px' }} src="https://open.spotify.com/embed/track/2Qn6WHJrY5Im82Jux8FboH?utm_source=generator&theme=0" width="250" height="100" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+                <Text as="h2" fontSize="lg" fontWeight="bold">Contact</Text>
                 <HStack spacing={3}>
                     <Link href="mailto:aditya.kantipudi@protonmail.com" isExternal>Email</Link>
                     <Link href="https://twitter.com/0xadi_k" isExternal>Twitter</Link>
